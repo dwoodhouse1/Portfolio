9d909e11d99b5b73e7b2dcc218796fae9133064e
dwoodhouse1 <148891593+dwoodhouse1@users.noreply.github.com>
2023-12-12 10:13:35 +0000

    JavaScript Code Snippet
    Added JavaScript code snippet section. This is ready for peer review

--- js/sectionToggle.js
+++ js/sectionToggle.js
@@ -1,28 +1,23 @@
 // Section Toggle
 const triangle = document.getElementsByClassName("toggle-section");
-const rotate = document.querySelector(".rotate");
+const rotate = document.getElementsByClassName("rotate");
 const sectionContent = document.getElementsByClassName("section-content");
-
-let isSectionToggled = document.getElementsByClassName("ishidden__section");
-console.log(triangle.length);
 
 for (let i = 0; i < triangle.length; i++)
 {
     sectionContent[i].style.display = "block";
-    console.log(sectionContent[i].style.display);
+
     triangle[i].addEventListener("click", function () {
-        this.classList.toggle("rotate");
-        console.log(sectionContent[i].style.display);
         
-        if(sectionContent[i].style.display === "block")
+        this.classList.toggle("rotate");
+        
+        if (sectionContent[i].style.display === "block")
         {
             sectionContent[i].style.display = "none";
-            console.log("should be none");
         }
         else
         {
            sectionContent[i].style.display = "block";
-           console.log("Should be block");
         }
 
     });
